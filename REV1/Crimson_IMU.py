2169721252943771c3132ad506045d00e35d33c8
rayan4444 <rayan.armani@gmail.com>
2019-07-26 19:35:59 +0800

    three lines on one graph

--- REV1/Crimson_IMU.py
+++ REV1/Crimson_IMU.py
@@ -1,25 +1,33 @@
 # reference code: https://www.thepoorengineer.com/en/arduino-python-plot/
 
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import struct
-
-from threading import Thread
-import serial
-import time
 import collections
+import struct
+import time
+from threading import Thread
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import serial
+import copy 
 
 
 class serialPlot:
     def __init__(
-        self, serialPort="COM11", serialBaud=115200, plotLength=100, dataNumBytes=2
-    ):
+        self, serialPort="COM11", serialBaud=115200, plotLength=100, dataNumBytes=2, numPlots=1):
         self.port = serialPort
         self.baud = serialBaud
         self.plotMaxLength = plotLength
         self.dataNumBytes = dataNumBytes
-        self.rawData = bytearray(dataNumBytes)
-        self.data = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.numPlots = numPlots
+        self.rawData = bytearray(numPlots*dataNumBytes)
+        self.dataType =None
+        if dataNumBytes == 2:
+            self.dataType = 'h'     # 2 byte integer
+        elif dataNumBytes == 4:
+            self.dataType = 'f'     # 4 byte float
+        self.data = []
+        for i in range(numPlots):   # give an array for each type of data and store them in a list
+            self.data.append(collections.deque([0] * plotLength, maxlen=plotLength))
         self.isRun = True
         self.isReceiving = False
         self.thread = None
@@ -30,16 +38,16 @@ class serialPlot:
         print(
             "Trying to connect to: "
             + str(serialPort)
-            + "at"
+            + " at "
             + str(serialBaud)
-            + "BAUD."
+            + " BAUD."
         )
         try:
-            self.serialConneciton = serial.Serial(
+            self.serialConnection = serial.Serial(
                 port=serialPort, baudrate=serialBaud, timeout=4
             )
 
-            print("Connected to" + str(serialPort) + "at" + str(serialBaud) + "BAUD.")
+            print("Connected to " + str(serialPort) + " at " + str(serialBaud) + " BAUD.")
         except:
             print("Failed to connect")
 
@@ -53,33 +61,33 @@ class serialPlot:
 
     def getSerialData(self, frame, lines, lineValueText, lineLabel, timeText):
         currentTimer = time.perf_counter()
-        self.plotTimer = int((currentTimer - self.previousTimer) * 1000)
+        self.plotTimer = int((currentTimer - self.previousTimer) * 1000)     # the first reading will be erroneous
         self.previousTimer = currentTimer
-        timeText.set_text("Plot Interval = " + str(self.plotTimer) + "ms")
-        value, = struct.unpack(
-            "h", self.rawData
-        )  # use 'f' for a float (or arduino double)
-        self.data.append(value)  # get the latest data point and append it to our array
-        lines.set_data(range(self.plotMaxLength), self.data)
-        lineValueText.set_text("[" + lineLabel + "] = " + str(value))
-        # self.csvDara.append(self.data[-1])
+        timeText.set_text('Plot Interval = ' + str(self.plotTimer) + 'ms')
+        privateData = copy.deepcopy(self.rawData[:])    # so that the 3 values in our plots will be synchronized to the same sample time
+        for i in range(self.numPlots):
+            data = privateData[(i*self.dataNumBytes):(self.dataNumBytes + i*self.dataNumBytes)]
+            value,  = struct.unpack(self.dataType, data)
+            self.data[i].append(value)    # we get the latest data point and append it to our array
+            lines[i].set_data(range(self.plotMaxLength), self.data[i])
+            lineValueText[i].set_text('[' + lineLabel[i] + '] = ' + str(value))
+        # self.csvData.append([self.data[0][-1], self.data[1][-1], self.data[2][-1]])
 
     def backgroundThread(self):
         time.sleep(1.0)
-        self.serialConneciton.reset_input_buffer()
+        self.serialConnection.reset_input_buffer()
         while self.isRun:
-            self.serialConneciton.readinto(self.rawData)
+            self.serialConnection.readinto(self.rawData)
             self.isReceiving = True
             #print(self.rawData)
 
     def close(self):
         self.isRun = False
         self.thread.join()
-        self.serialConneciton.close()
-        print("Disconnected...")
+        self.serialConnection.close()
+        print('Disconnected...')
         # df = pd.DataFrame(self.csvData)
         # df.to_csv('/home/rikisenia/Desktop/data.csv')
-
 
 def main():
 
@@ -87,10 +95,10 @@ def main():
     baudRate = 115200
     maxPlotLength = 100
     dataNumBytes = 2  # use 4 if sending float/double data typer from arduino
-
-    s = serialPlot(portName, baudRate, maxPlotLength, dataNumBytes)
-    s.readSerialStart()
-
+    numPlots = 3  
+    s = serialPlot(portName, baudRate, maxPlotLength, dataNumBytes, numPlots)   # initializes all required variables
+    s.readSerialStart()                                               # starts background thread
+ 
     # plotting
     pltInterval = 50  # Period at whihc the plot animation updates [ms]
     xmin = 0
@@ -103,23 +111,21 @@ def main():
     ax.set_xlabel("time")
     ax.set_ylabel("AnalogRead Value")
 
-    lineLabel = "Potentiometer Value"
-    timeText = ax.text(0.5, 0.95, "", transform=ax.transAxes)
-    lines = ax.plot([], [], label=lineLabel)[0]
-    lineValueText = ax.text(0.50, 0.90, '', transform=ax.transAxes)
-    anim = animation.FuncAnimation(
-        fig,
-        s.getSerialData,
-        fargs=(lines, lineValueText, lineLabel, timeText),
-        interval=pltInterval,
-    )  # fargs has to be a tuple
-
+    lineLabel = ['X', 'Y', 'Z']
+    style = ['r-', 'c-', 'b-']  # linestyles for the different plots
+    timeText = ax.text(0.70, 0.95, '', transform=ax.transAxes)
+    lines = []
+    lineValueText = []
+    for i in range(numPlots):
+        lines.append(ax.plot([], [], style[i], label=lineLabel[i])[0])
+        lineValueText.append(ax.text(0.70, 0.90-i*0.05, '', transform=ax.transAxes))
+    anim = animation.FuncAnimation(fig, s.getSerialData, fargs=(lines, lineValueText, lineLabel, timeText), interval=pltInterval)    # fargs has to be a tuple
+ 
     plt.legend(loc="upper left")
     plt.show()
-
+ 
     s.close()
 
 
 if __name__ == "__main__":
     main()
-
